4b3f8ba61461af5e6d5153479941d113d9f18727
Gokul Prathin <gokulprathin8@gmail.com>
2021-04-18 00:34:18 +0530

    Added Models

--- fccPy/settings.py
+++ fccPy/settings.py
@@ -37,7 +37,17 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'crispy_forms',
+
+    'customers',
+    'products',
+    'profiles',
+    'reports',
+    'sales'
 ]
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -119,6 +129,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    BASE_DIR / 'static'
+]
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
--- sales/models.py
+++ sales/models.py
@@ -1,3 +1,43 @@
 from django.db import models
+from products.models import Products
+from customers.models import Customer
+from profiles.models import Profile
+from django.utils import timezone
 
-# Create your models here.
+class Position(models.Model):
+    product = models.ForeignKey(Products, on_delete=models.CASCADE)
+    quantity = models.PositiveBigIntegerField()
+    price = models.FloatField(blank=True)
+    created = models.DateTimeField(blank=True)
+
+    def save(self, args, **kwargs):
+        self.price = self.product.price * self.quantity
+        return super().save(*args, **kwargs)
+
+    def __str__(self):
+        return f"id: {self.id}, product: {self.product.name}"
+
+class Sale(models.Model):
+    transcation_id = models.CharField(max_length=255, blank=True)
+    positions = models.ManyToManyField(Position)
+    total_price = models.FloatField(blank=True)
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    salesman = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(blank=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.total_price
+
+    def save(self, *args, **kwargs):
+        if self.transcation_id == "":
+            self.transcation_id == ''
+        if self.created_at is None:
+            self.created_at = timezone.now()
+        return super().save(*args, **kwargs)
+
+    def get_positions(self):
+        return self.positions.all()
+
+class CSV(models.Model):
+    pass
